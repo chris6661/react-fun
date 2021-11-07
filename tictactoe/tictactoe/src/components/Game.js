ffba8c204cc69501701b364b52d63771527f63be
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-11-07 11:13:42 -0600

    finished, added in time/turn review

--- tictactoe/tictactoe/src/components/Game.js
+++ tictactoe/tictactoe/src/components/Game.js
@@ -2,37 +2,56 @@ import React, {useState} from 'react'
 import { calculateWinner } from '../helpers';
 import Board from './Board'; 
 
+const styles = {
+    width: "200px", 
+    margin: '20px auto' 
+}; 
 
 const Game = () => {
 
-    const [board, setBoard] = useState(Array(9).fill(null));
-
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXIsNext] = useState(true);
-    const winner = calculateWinner(board);
+    const winner = calculateWinner(history[stepNumber]);
 
     const handleClick = i => {
-        const boardCopy = [...board];
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[timeInHistory.length - 1];
+        const squares = [...current];
+
         //if user clicks on a square that is already filled or if game is won, do nothing
-        if (winner || boardCopy[i]) return; 
+        if (winner || squares[i]) return; 
 
         //put x or o in clicked square
-        boardCopy[i] = xIsNext ? 'X' : 'O';
-        setBoard(boardCopy); 
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(timeInHistory.concat([squares]));
+        setStepNumber(timeInHistory.length);
         setXIsNext(!xIsNext);
     }
 
-    const jumpTo = () => { 
+    const jumpTo = step => { 
+        setStepNumber(step);
+        setXIsNext(step % 2 === 0);
+    };
 
-    }
-
-    const renderMoves = () => {
-    
-    }
+    const renderMoves = () => (
+        history.map((step, move) => {
+            const destination = move ? `Go to move#${move}`: 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            )
+        })
+    )
 
     return (
         <>
-            <Board squares={board} onClick={handleClick} />
-            
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div style={styles}>
+                <p>{winner ? 'Winner: ' + winner : 'Next Player ' + (xIsNext ? 'X' : 'O')}</p>
+                {renderMoves()}
+            </div>
         </>
     )
 }
